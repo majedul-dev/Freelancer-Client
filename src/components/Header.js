955f4e1bf1e01e027b60b6fef132d14f0ee965e9
majedul-dev <majedulislam956@gmail.com>
2021-03-04 21:04:45 +0600

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div style={{ padding: "1rem 0" }}>
+      <h2 style={{ color: "#1890ff", fontSize: "2rem" }}>Logo</h2>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/PageOne.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Button, Input } from "antd";
+import img from "../assets/1.png";
+
+const PageOne = () => {
+  return (
+    <div
+      style={{
+        marginTop: "8rem",
+        display: "flex",
+        justifyContent: "space-between",
+        flex: "1",
+      }}
+    >
+      <div>
+        <h1 style={{ fontSize: "3.5rem" }}>
+          Welcome to your Digital Front Door!
+        </h1>
+        <p style={{ color: "#999", fontSize: "1.2rem" }}>
+          Where you will <strong>help youe doctor</strong> out by providing
+          necessary information to have a <strong>successful visit</strong>
+        </p>
+        <Input
+          placeholder="Enter your mobile number"
+          style={{ width: "15rem", display: "block", marginBottom: "1rem" }}
+        />
+        <Button type="primary" s>
+          Submit
+        </Button>
+      </div>
+      <img src={img} alt="" style={{ height: "30rem", marginTop: "-5rem" }} />
+    </div>
+  );
+};
+
+export default PageOne;
